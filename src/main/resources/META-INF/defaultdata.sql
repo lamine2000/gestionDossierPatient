36fd7dfb25d1ad14fdc4566a380d936b5e0c351f
lamine <lemzogueye.lg72@gmail.com>
2023-01-29 16:08:31 +0000

    removed unnecessary database request since all the previous ones are persisted

--- src/main/resources/META-INF/defaultdata.sql
+++ src/main/resources/META-INF/defaultdata.sql
@@ -1,9 +1,1 @@
-create database manssi;
 use manssi;
-create table test(
-    id integer auto_increment primary key,
-    name varchar(255),
-    surname varchar(255)
-);
-
-insert into test value (1, 'lamine', 'gueye');
