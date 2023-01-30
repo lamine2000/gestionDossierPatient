9573a44b171d98b36cf9beef445e7a6f4ae39d53
lamine <lemzogueye.lg72@gmail.com>
2023-01-30 04:47:31 +0000

    added endpoint for login

--- src/main/java/com/gestion_dossier_patient/gestiondossierpatient/resources/UserResource.java
+++ src/main/java/com/gestion_dossier_patient/gestiondossierpatient/resources/UserResource.java
@@ -1,21 +1,27 @@
 package com.gestion_dossier_patient.gestiondossierpatient.resources;
 
 import com.gestion_dossier_patient.gestiondossierpatient.entities.User;
+import com.gestion_dossier_patient.gestiondossierpatient.resources.vm.LoginVM;
 import com.gestion_dossier_patient.gestiondossierpatient.services.UserService;
+import jakarta.annotation.security.DenyAll;
+
+import jakarta.annotation.security.PermitAll;
 import jakarta.inject.Inject;
+import jakarta.validation.constraints.NotNull;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
 import java.util.List;
 
-@Path("/user")
+@Path("/users")
 public class UserResource {
 
     @Inject
     private UserService userService;
 
     @GET
+    @Path("/all")
     @Produces(MediaType.APPLICATION_JSON)
     public Response getAllUsers() {
         List<User> users = userService.findAll();
@@ -25,20 +31,20 @@ public class UserResource {
     @GET
     @Path("/{id}")
     @Produces(MediaType.APPLICATION_JSON)
+    @DenyAll
     public Response getUser(@PathParam("id") Long id) {
         User user = userService.findById(id);
-        if (user != null) {
+        if (user != null)
             return Response.ok(user).build();
-        } else {
-            return Response.status(Response.Status.NOT_FOUND).build();
-        }
+        return Response.status(Response.Status.NOT_FOUND).build();
     }
 
     @POST
     @Path("/register")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Response createUser(User user) {
+    @PermitAll
+    public Response createUser(@NotNull User user) {
         user = userService.create(user);
         return Response.ok(user).build();
     }
@@ -47,13 +53,11 @@ public class UserResource {
     @Path("/{id}")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Response updateUser(@PathParam("id") Long id, User user) {
+    public Response updateUser(@PathParam("id") Long id, @NotNull User user) {
         user = userService.update(id, user);
-        if (user != null) {
+        if (user != null)
             return Response.ok(user).build();
-        } else {
-            return Response.status(Response.Status.NOT_FOUND).build();
-        }
+        return Response.status(Response.Status.NOT_FOUND).build();
     }
 
     @DELETE
@@ -61,10 +65,22 @@ public class UserResource {
     @Produces(MediaType.APPLICATION_JSON)
     public Response deleteUser(@PathParam("id") Long id) {
         boolean success = userService.delete(id);
-        if (success) {
+        if (success)
             return Response.ok().build();
-        } else {
-            return Response.status(Response.Status.NOT_FOUND).build();
-        }
+        return Response.status(Response.Status.NOT_FOUND).build();
+    }
+
+    //login user
+    @POST
+    @Path("/login")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @PermitAll
+    public Response login(@NotNull LoginVM user) {
+        //make the login method return the token
+        final String token = userService.login(user);
+        if (token != null)
+            return Response.ok(token).build();
+        return Response.status(Response.Status.NOT_FOUND).build();
     }
 }
