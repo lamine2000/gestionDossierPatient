28d95391e2c04cf4734e8828b675b2dd65611347
lamine <lemzogueye.lg72@gmail.com>
2023-01-30 04:50:00 +0000

    added methods needed to -know type of user based on their username (Doctor, Patient, Emergency Doctor) -login

--- src/main/java/com/gestion_dossier_patient/gestiondossierpatient/services/UserService.java
+++ src/main/java/com/gestion_dossier_patient/gestiondossierpatient/services/UserService.java
@@ -1,12 +1,19 @@
 package com.gestion_dossier_patient.gestiondossierpatient.services;
 
+import com.gestion_dossier_patient.gestiondossierpatient.entities.Doctor;
+import com.gestion_dossier_patient.gestiondossierpatient.entities.EmergencyDoctor;
+import com.gestion_dossier_patient.gestiondossierpatient.entities.Patient;
 import com.gestion_dossier_patient.gestiondossierpatient.entities.User;
+import com.gestion_dossier_patient.gestiondossierpatient.resources.vm.LoginVM;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.transaction.Transactional;
+import org.apache.commons.codec.digest.DigestUtils;
 
+import java.util.Base64;
 import java.util.List;
+import java.util.Optional;
 
 @ApplicationScoped
 public class UserService {
@@ -50,5 +57,90 @@ public class UserService {
 
         entityManager.remove(existingUser);
         return true;
+    }
+
+    //get by username
+    public Optional<User> findByUsername(String username) {
+        return entityManager
+            .createQuery("SELECT u FROM User u WHERE u.username = :username", User.class)
+            .setParameter("username", username)
+            .getResultStream()
+            .findFirst();
+    }
+
+    //is user
+    public boolean isUser(String username) {
+        return findByUsername(username).isPresent();
+    }
+
+    //is patient
+    public boolean isPatient(String username) {
+        Optional<User> user = findByUsername(username);
+
+        if(user.isEmpty())
+            return false;
+
+        //check if there is a patient with this user
+        return entityManager
+            .createQuery("SELECT p FROM Patient p WHERE p.user = :user", Patient.class)
+            .setParameter("user", user.get())
+            .getResultStream()
+            .findFirst()
+            .isPresent();
+    }
+
+    //is doctor
+    public boolean isDoctor(String username) {
+        Optional<User> user = findByUsername(username);
+
+        if(user.isEmpty())
+            return false;
+
+        //check if there is a doctor with this user
+        return entityManager
+            .createQuery("SELECT d FROM Doctor d WHERE d.user = :user", Doctor.class)
+            .setParameter("user", user.get())
+            .getResultStream()
+            .findFirst()
+            .isPresent();
+    }
+
+    //is emergency doctor
+    public boolean isEmergencyDoctor(String username) {
+        Optional<User> user = findByUsername(username);
+
+        if(user.isEmpty())
+            return false;
+
+        //check if there is a emergency doctor with this user
+        return entityManager
+            .createQuery("SELECT ed FROM EmergencyDoctor ed WHERE ed.user = :user", EmergencyDoctor.class)
+            .setParameter("user", user.get())
+            .getResultStream()
+            .findFirst()
+            .isPresent();
+    }
+
+    //login
+    public String login(LoginVM user) {
+        final String encryptedPassword = DigestUtils.sha256Hex(user.getPassword());
+        user.setPassword(encryptedPassword);
+
+        Optional<User> existingUser = findByUsername(user.getUsername());
+
+        if(existingUser.isEmpty())
+            return null;
+
+        final User eu = existingUser.get();
+        final String username = eu.getUsername();
+        final String password = eu.getPassword();
+        final String decodedPayload = username.concat(":").concat(password);
+        System.err.println(decodedPayload);
+
+        if(eu.getPassword().equals(user.getPassword()))
+            return Base64.getEncoder()
+                .encodeToString(decodedPayload.getBytes());
+
+        return null;
     }
 }
